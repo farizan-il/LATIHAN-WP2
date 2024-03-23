feb73c7605589bd5887fbbfbe2e0a633f5f38365
farizan-il <farizanilham7@gmail.com>
2024-03-23 21:03:32 +0700

    upload file tugas wp2

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <h1>Haloo Farizan... Yuk Belajar Web Programming 2 !!</h1>
+    <p> Nilai A = <?= $nilai1; ?> </p>
+    <p> Nilai B = <?= $nilai2; ?> </p>
+    <p> Jika di jumlahkan hasilnya <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?> </p>
+</body>
+
+</html>
